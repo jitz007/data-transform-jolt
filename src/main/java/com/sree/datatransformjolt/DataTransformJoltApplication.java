77166e64d53ab1910f6b8ad8d2a5a5d16702fd9a
jitz007 <63137182+jitz007@users.noreply.github.com>
2022-02-18 14:55:58 +0530

    Init Commit

---
+++ src/main/java/com/sree/datatransformjolt/DataTransformJoltApplication.java
@@ -0,0 +1,13 @@
+package com.sree.datatransformjolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTransformJoltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataTransformJoltApplication.class, args);
+	}
+
+}
